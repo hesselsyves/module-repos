90016142833d4372bf8635070cca441a600f6be8
Yves Hessels <yves.hessels@acerta.be>
2025-10-08 14:31:46 +0200

    added the possibility to symlink only one directory by passing its name as argument

--- symlink-local.sh
+++ symlink-local.sh
@@ -2,13 +2,25 @@
 SOURCE_DIR="/home/yves/sites"
 TARGET_DIR="docroot/modules/acerta"
 
-for module in "$TARGET_DIR"/*; do
-  name=$(basename "$module")
+if [ -n "$1" ]; then
+  name="$1"
   source_name="drupal-${name//_/-}"
   if [ -d "$SOURCE_DIR/$source_name" ]; then
-    rm -rf "$module"
+    rm -rf "$TARGET_DIR/$name"
     ln -s "$SOURCE_DIR/$source_name" "$TARGET_DIR/$name"
     echo "Symlinked $name from $SOURCE_DIR/$source_name"
+  else
+    echo "Source directory $SOURCE_DIR/$source_name does not exist."
   fi
-done
+else
+  for module in "$TARGET_DIR"/*; do
+    name=$(basename "$module")
+    source_name="drupal-${name//_/-}"
+    if [ -d "$SOURCE_DIR/$source_name" ]; then
+      rm -rf "$module"
+      ln -s "$SOURCE_DIR/$source_name" "$TARGET_DIR/$name"
+      echo "Symlinked $name from $SOURCE_DIR/$source_name"
+    fi
+  done
+fi
 
